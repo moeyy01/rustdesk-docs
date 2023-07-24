3a653e77808ad884d8f76449a11c5286ec8c5e72
moeyys <57945487+moeyy01@users.noreply.github.com>
2023-07-24 20:12:48 +0800

    1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress';
 
 // refer https://vitepress.vuejs.org/config/introduction for details
 export default defineConfig({
-  base: '/',
+  base: '/rustdesk/',
   lang: 'cn-ZH',
   title: 'Moeyy Rustdesk',
   description: '由 Moeyy 提供的 RustDesk 服务',
