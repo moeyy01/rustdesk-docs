1c1c5d5c996e1dc4dbb9e47d77ea24d5c69e5a68
moeyys <57945487+moeyy01@users.noreply.github.com>
2023-07-24 19:20:16 +0800

    upload

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vitepress';
+
+// refer https://vitepress.vuejs.org/config/introduction for details
+export default defineConfig({
+  base: '/',
+  lang: 'cn-ZH',
+  title: 'Moeyy Rustdesk',
+  description: '由 Moeyy 提供的 RustDesk 服务',
+
+  // themeConfig: {
+  //   // nav: [
+  //   //   { text: '介绍', link: '/welcome' },
+  //   //   { text: '下载', link: '/download' },
+  //   //   { text: '配置', link: '/setting' },
+  //   //   { text: '运行', link: '/run' },
+
+  //     // {
+  //     //   text: 'Dropdown Menu',
+  //     //   items: [
+  //     //     { text: 'Item A', link: '/item-1' },
+  //     //     { text: 'Item B', link: '/item-2' },
+  //     //     { text: 'Item C', link: '/item-3' },
+  //     //   ],
+  //     // },
+
+  //     // ...
+  //   // ],
+
+  //   sidebar: [
+  //     {
+  //       // text: 'Guide',
+  //       // items: [
+  //       //   { text: '介绍', link: '/welcome' },
+  //       //   { text: '下载', link: '/download' },
+  //       //   { text: '配置', link: '/setting' },
+  //       //   { text: '运行', link: '/run' },
+  //       //   // ...
+  //       // ],
+  //     },
+  //   ],
+  // },
+});
---
+++ docs/index.md
@@ -0,0 +1,42 @@
+---
+sidebar: true
+contributors: true
+---
+
+# 由 Moeyy 提供的 RustDesk 服务
+
+RustDesk 是一款开源的远程桌面软件，支持多个主流平台。我们最初计划将其用于远程技术支持，不过由于其功能强大，目前也对部分用户开放试用。
+
+## 使用限制
+
+- **严禁** 将此服务用于非法控制他人设备、窃取信息、电信诈骗等非法用途
+- 原则上不支持使用 TCP 隧道功能，请勿滥用该功能
+- 文件传输功能同理，请勿滥用（频繁传输超大文件等）
+
+
+## 下载软件
+
+您可以选用 [RustDesk 官方发布的版本](https://github.com/rustdesk/rustdesk/releases/latest) 或是 [Moeyy 网盘](https://moeyy.cn/down/rustdesk)。
+
+
+## 配置指南 {#configure}
+
+官方客户端需配置 **ID/中继服务器** 以连接到 SakuraFrp。请确保 **复制粘贴** 所有内容，避免因输入错误而无法连接。
+
+| 配置项 | 内容 |
+| --- | --- |
+| ID 服务器 | `rustdesk.moeyy.cn` |
+| API 服务器 | `rustdesk.moeyy.cn` |
+
+
+### 桌面客户端
+
+![](https://cdn.moeyy.cn/2022/11/SXO0TQ1ISOH@D6Q2JU6.png/moeyy_webp)
+
+### IOS 客户端
+
+![](https://cdn.moeyy.cn/img/202307241913403.png/moeyy_webp)
+
+
+
+完成后，就可以正常使用 RustDesk 远程管理您的设备了。目前您只能管理登录到同一个账户的设备。
